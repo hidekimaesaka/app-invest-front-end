6f03598615ba750aa8e4b06682223d055f900857
hidekimsk <Lucasmsk11@gmail.com>
2024-09-23 13:53:10 -0300

    inital commit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,21 @@
+import './Home.css';
+
+export default function Home(){
+    return (
+        <div className='home-page'>
+            <div className="page-header"></div>
+                <main className='home-container'>
+            <div className="banner-text">
+                <h1>Welcome to App Invest!</h1>
+                <h2 className='banner-sub-text'>Start managing your investments wallet right now!</h2>
+                </div>
+            <div className="action-buttons">
+                <a href="">Create Account</a>        
+                <a href="">Login</a>        
+            </div>
+        </main>
+            <div className="page-footer"></div>
+        </div>
+
+    );
+}
